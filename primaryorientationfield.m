c76761509bb52838ca8599fc04b245497b8de109
Wai-Tsun Yeung <williamywt0624@gmail.com>
2023-03-06 22:31:13 +0800

    Added gaussian filter feature.

--- primaryorientationfield.m
+++ primaryorientationfield.m
@@ -2,16 +2,18 @@ function [newimage,lines] = primaryorientationfield(image,lines)
 %Generate primary orientation field from 2D or 3D images.
 %Cellular tomograms used typically need their colours reverted before
 %processing to achieve optimal result.
-%This programming code is adapted from this paper:
-%Sandberg, K.: Curve enhancement using orientation fields. In Bebis, G.
-%(ed.) ISVC 2009, Part 1. LNCS, vol. 5875, pp. 564-575. Springer,
-%Heidelberg(2009)
 [r,c,h] = size(image);
 newimage = zeros(r,c,h,length(lines));
 simage = im2single(image);
 line = cell2mat(permute(lines(:,1),[2 3 1]));
 [l,~,~] = size(line);
 line = line./padarray(sum(sum(line,1),2),[l-1 l-1 0],'replicate','post');
+l = length(lines);
+gauss_sigma = floor(floor(l * 0.5) * 0.5);
+gauss_filter ...
+    = exp(-(-floor(l * 0.5):floor(l * 0.5)).^2*0.5./(gauss_sigma.^2)) ...
+    ./(gauss_sigma*sqrt(2*pi));
 for ii = 1:length(lines)
     newimage(:,:,:,ii) = imfilter(simage,line(:,:,ii));
 end
+newimage = newimage.*permute(gauss_filter,[1,3,4,2]);
--- primaryorientationfield3.m
+++ primaryorientationfield3.m
@@ -2,10 +2,6 @@ function [newimage,lines] = primaryorientationfield3(image,lines)
 %Generate primary orientation field from 2D or 3D images.
 %Cellular tomograms used typically need their colours reverted before
 %processing to achieve optimal result.
-%This programming code is adapted from this paper:
-%Sandberg, K.: Curve enhancement using orientation fields. In Bebis, G.
-%(ed.) ISVC 2009, Part 1. LNCS, vol. 5875, pp. 564-575. Springer,
-%Heidelberg(2009)
 [r,c,h] = size(image);
 newimage = zeros(r,c,h,length(lines),'single');
 image = im2single(image);
@@ -16,3 +12,9 @@ line = line./padarray(sum(sum(sum(line,1),2),3),[l-1 l-1 l-1 0],...
 for ii = 1:length(lines)
     newimage(:,:,:,ii) = imfilter(image,line(:,:,:,ii));
 end
+l = length(lines);
+gauss_sigma = floor(floor(l * 0.5) * 0.5);
+gauss_filter ...
+    = exp(-(-floor(l * 0.5):floor(l * 0.5)).^2*0.5./(gauss_sigma.^2)) ...
+    ./(gauss_sigma*sqrt(2*pi));
+newimage = newimage.*permute(gauss_filter,[1,3,4,2]);
